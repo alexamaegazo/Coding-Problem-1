52dd663fff81eec20e79f835cc5135ca2902d04d
alexamaegazo <alexamaegazo5@gmail.com>
2024-11-04 18:31:58 +0800

    Initial commit with userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,13 @@
+//Collect the following user information:
+
+let fullname = "Alexa Mae V. Gazo";
+console.log("Full Name:", fullname);
+
+let age = "19";
+console.log("Age:", age);
+
+let favNumber = "18";
+console.log("Favorite Number:", favNumber);
+
+let favColor = "Green";
+console.log("Favorite Color:", favColor);
